addbda02e54418efc4588d59e178a0b181d8d6f1
Jonathan Truong <88113446+TruongJon@users.noreply.github.com>
2022-03-28 01:42:53 -0400

    Working implementation of Zip Lookup WebApp

--- backend/server.py
+++ backend/server.py
@@ -1,18 +1,34 @@
-from re import X
 import pigLatin as pl
+import json
+import pip._vendor.requests
+from pip._vendor.requests.auth import HTTPBasicAuth
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
+# Authentication info for API calls to ZIP API US
+email = "truong.jo@northeastern.edu"
+password = "zeropassword"
+key = "e5b77470f2671c5662e32df58def1a44"
 
 # create_phrase API route
 @app.route('/create_phrase', methods=['GET', 'POST'])
 def create_phrase():
     data = request.json
     name = data.get("name")
-    zip = data.get("zip")  
     tokens = name.split(" ")
-    county = "x"
-    population = "y"
+    zip = data.get("zip")  
+
+    countyReq = pip._vendor.requests.get("https://service.zipapi.us/zipcode/county/{}?X-API-KEY={}"
+    .format(zip, key), auth=HTTPBasicAuth(email, password))
+    countyjson = json.loads(countyReq.text)
+    county = countyjson.get("data").get("county")
+
+    populationReq = pip._vendor.requests.get("https://service.zipapi.us/population/zipcode/{}?X-API-KEY={}&fields=male_population,female_population"
+    .format(zip, key), auth=HTTPBasicAuth(email, password))
+    populationjson = json.loads(populationReq.text)
+    population = population.json.get("data").get("population")
+
     phrase = (pl.pigLatin(tokens[0]) + " " + pl.pigLatin(tokens[1]) + 
     "'s is in {} and has a population of {}".format(county, population))
     return {
